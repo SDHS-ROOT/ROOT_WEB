58c447374a359e6b0ce15193e6a07e37987de23b
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2020-12-16 04:17:48 +0900

    Create writer.php

---
+++ writer.php
@@ -0,0 +1,39 @@
+<!-- 글 입력 페이지 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>게시판</title>
+    <link rel="stylesheet" type="text/css" href="/ROOT_WEB/css/style.css" />
+</head>
+<body>
+    <div id="board_write">
+        <h1><a href="/">자유게시판</a></h1>
+        <h4>글을 작성하는 공간입니다.</h4>
+        <div id="write_area">
+            <form action="write_input.php" method="post">
+                <!-- <div id="bt_class">
+                    <input type="radio" name="class" value="공지">공지
+                    <input type="radio" name="class" value="문의">문의
+                </div> -->
+                <div id="in_title">
+                    <!-- required = 필수입력항목 설정 -->
+                    <textarea name="title" id="utitle" cols="55" rows="1" placeholder="제목" maxlength="100" required></textarea>
+                </div>
+                <div class="wi_line"></div>
+                <div id="in_name">
+                    <textarea name="author" id="uauthor" cols="55" rows="1" placeholder="글쓴이" maxlength="100" require></textarea>
+                </div>
+                <div class="wi_line"></div>
+                <div id="in_content">
+                    <textarea name="content" id="ucontent" placeholder="내용" required></textarea>
+                </div>
+                <div id="bt_se">
+                    <button type="submit">글 작성</button>
+                </div>
+            </form>   
+        </div>
+    </div>
+</body>
+</html>
