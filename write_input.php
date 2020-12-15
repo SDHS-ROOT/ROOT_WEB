7b13fc3a47092fc42ae3706e8dce1845bb346479
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2020-12-16 07:09:17 +0900

    Update write_input.php

--- write_input.php
+++ write_input.php
@@ -13,12 +13,11 @@
     $author = $_POST['author'];
     $title = $_POST['title'];
     $content = $_POST['content'];
-    $date = date('Y-m-d H:i');
     if($author && $title && $content) {
         $mqq = mq("alter table board auto_increment = 1"); // auto_increment 값 초기화
         // auto_increment 속성의 id는 primary key이기 때문에 값이 중복될 수 없음. 그래서 중복되지 않는 값을 찾아 그 값부터 시작하게 됨.
 
-        $sql = mq("insert into board(title, author, content, date) values('".$title."','".$author."','".$content."','".$date."')");
+        $sql = mq("insert into board(title, author, content) values('".$title."','".$author."','".$content."')");
         // $sql = mq("insert into board(class, title, author, content, date) values('".$userclass."','".$title."','".$author."','".$content."','".$date."')");
         echo "<script>
         alert('글쓰기 완료되었습니다.');
