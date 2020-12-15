c154834b0119d14f6ea942bf0f5f17c3c0e8ad3c
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2020-12-16 06:24:59 +0900

    Create modify_input.php

---
+++ modify_input.php
@@ -0,0 +1,14 @@
+<!-- 글 수정 후 데이터베이스에 입력 -->
+<?php 
+    include $_SERVER['DOCUMENT_ROOT']."/ROOT_WEB/db.php";
+
+    $bno = $_GET['id'];
+    $author = $_POST['author'];
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $date = date('Y-m-d H:i:s');
+    $sql = mq("update board set author='".$author."', title='".$title."', content='".$content."', date='".$date."' where id='".$bno."'");
+?>
+
+<script type="text/javascript">alert("수정되었습니다."); </script>
+<meta http-equiv="refresh" content="0 url=read.php?id=<?php echo $bno; ?>" />
