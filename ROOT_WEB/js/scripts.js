0127bd97ae4d1f05623375bb66c03f1e05687402
Ch10316 <andrewlime04@naver.com>
2020-12-17 22:01:35 +0900

    last

---
+++ ROOT_WEB/js/scripts.js
@@ -0,0 +1,34 @@
+/*!
+    * Start Bootstrap - Freelancer v6.0.5 (https://startbootstrap.com/theme/freelancer)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+    */
+    (function($) {
+    "use strict"; // strict을 사용한다. --> 여러 브라우저에서도 잘 사용되게 하기 위해서
+  
+    // 아래로 스크롤을 하면 메뉴바에 크기가 줄어든다.
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    navbarCollapse();
+
+    // 페이지 전체에 적용
+    $(window).scroll(navbarCollapse);
+  
+    // 가입신청 페이지 적용
+    $(function() {
+      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+      }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+      }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+      });
+    });
+  
+  })(jQuery); // strict 사용을 종료
+  
