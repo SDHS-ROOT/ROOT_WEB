13429282b698402f8103dc338d05eef7c243686c
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2020-12-18 05:09:46 +0900

    f apply

--- write_input.php
+++ write_input.php
@@ -2,14 +2,6 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT']."/ROOT_WEB/db.php";
 
-    // 각 변수에 write.php에서 input name값들을 저장
-    // $userclass = $_POST['class'];
-    // if($userclass == 1) { // $userclass가 1이면(True) 관리자 권한. 즉, 공지글
-
-    // }
-    // else { // 1이 아닌 값이라면 일반 사용자. 즉, 문의/질문글
-
-    // }
     $author = $_SESSION['username'];
     $title = $_POST['title'];
     $content = $_POST['content'];
@@ -26,7 +18,6 @@
     else {
         echo "<script>
         alert('글쓰기에 실패했습니다.');
-        history.back(); </script>";
-        // history.back() = 이전 페이지로 이동
+        history.back(); </script>"; // history.back() = 이전 페이지로 이동
     }
 ?>
