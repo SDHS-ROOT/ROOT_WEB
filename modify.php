9d66c513cc1054b8ece7d0d52739842a60c886b8
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2020-12-18 17:46:00 +0900

    f

--- modify.php
+++ modify.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>게시판</title>
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/board.css" />
 </head>
 <body>
     <div id="board_write">
