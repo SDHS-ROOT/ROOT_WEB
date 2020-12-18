fb44feb92178c4cb4eac8e8f82e8ac55ede80058
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2020-12-18 18:05:14 +0900

    f back

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="index.php">게시판</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="가입신청.html">가입신청</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="contact.php">로그인</a></li>
-                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="member.php">회원가입</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="member.html">회원가입</a></li>
                 </ul>
             </div>
         </div>
